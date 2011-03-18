063fa8d45ce99ac2c686673a61cc423c3631ad74
jach <jach@wayf.dk>
2011-03-18 10:20:52 +0000

    Initial import

---
+++ templates/accessblock.php
@@ -0,0 +1,15 @@
+<?php
+
+$this->data['header'] = $this->t('accessblocker_header');
+
+$this->includeAtTemplateBase('includes/header.php');
+
+?>
+
+<h1><?php echo $this->t('accessblocker_header'); ?></h1>
+
+
+<?php
+
+$this->includeAtTemplateBase('includes/footer.php');
+?>
